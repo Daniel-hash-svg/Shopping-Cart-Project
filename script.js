2e77d29bcfeffb6218d0e7396213483fd6852469
Daniel Ceci <danielceci96@gmail.com>
2021-04-23 23:23:20 -0300

    requisito 5

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 const classItemCart = '.cart__items'; // isso aqui é pra evitar problema de Lint.
+// requisito 5 eu tive a ajuda do meu colega Alexandre Damasceno. Link do PR dele: https://github.com/tryber/sd-010-b-project-shopping-cart/blob/d5e394a69f6aac16e9e4d70963e8d3177fc0e4ce/script.js
+function soma() { // é pro requisito 5
+  const span = document.querySelector('.total-price'); 
+  const li = document.querySelectorAll('.cart__item');
+  let sum = 0;
+  for (let i = 0; i < li.length; i += 1) {
+    const valor = li[i].innerText.substring(li[i].innerText.indexOf('$') + 1);
+    sum += parseFloat(valor);
+  }
+  span.innerHTML = Math.round(sum * 100) / 100;
+}
+
 function eraseAll() { // requisito 6
   const removeButton = document.getElementsByClassName('empty-cart')[0];// botão que esvazia o carrinho
   removeButton.addEventListener('click', () => { // fazer essa HOF aqui no addEventListener me ajudou a resolver problema de lint. Fazendo assim, eu chamo a função eraseAll usando o addEventListener dentro da própria eraseAll. 
@@ -6,21 +18,25 @@ function eraseAll() { // requisito 6
     
       const ol = document.querySelector(classItemCart); // assim eu alcanço o OL usando a const que eu defini na linha 1, ao invés de usar o .cart_items nesse querySelector.
    localStorage.setItem('produto', ol.innerHTML);
-    });
+      soma();
+    }); 
 }
-
+function cartItemClickListener(event) { // essa vem de um Click também.
+  event.target.remove();// o event.target alcança o elemento que começou o evento(Nesse caso, seria o LI). Então ao clicar em LI, eu chamo essa função e esta função remove este LI.
+    const ol = document.querySelector(classItemCart);
+    // arraySum.splice(arraySum.indexOf(Number(event.target.innerText.split('$')[1])), 1); // essa é a parte do requisito 5, Pra reduzir o preço.
+    localStorage.setItem('produto', ol.innerHTML);
+    soma(); // quando apaga algum LI tb a soma precisa ser refeita pra diminuir o valor
+} 
 // a função abaixo usei para o requisito 4. Para esta resolução, tive a ajuda do colega João Herculano.
 // Link do PR dele : https://github.com/tryber/sd-010-b-project-shopping-cart/pull/88/commits/a2d11f39b333e0c0898502bccccaa4f18cff8425
-  function storage() {
+   function storage() {
     const armazenar = localStorage.getItem('produto');
-    document.querySelector(classItemCart).innerHTML = armazenar; // usando o innerHTML que manteve os números na lista. Usando innerText esses números sumiam.
+    
+     document.querySelector(classItemCart).innerHTML = armazenar; // usando o innerHTML que manteve os números na lista. Usando innerText esses números sumiam.
     console.log(armazenar);
-  }
-function cartItemClickListener(event) { // 
-  event.target.remove();// o event.target alcança o elemento que começou o evento(Nesse caso, seria o LI). Então ao clicar em LI, eu chamo essa função e esta função remove este LI.
-   const ol = document.querySelector(classItemCart);
-    localStorage.setItem('produto', ol.innerHTML);
-}
+   }
+
 function createCartItemElement({ id: sku, title: name, price: salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
@@ -30,19 +46,21 @@ function createCartItemElement({ id: sku, title: name, price: salePrice }) {
    
   return li;
  }
-
-const addToCard = async (sku) => {
+  
+const addToCard = async (sku) => { // essa é depois q cliquei no botão verde.
   const response = await fetch(`https://api.mercadolibre.com/items/${sku}`);
   const jsonIds = await response.json();// melhor fazer dessa forma do que usando then()
   const clickItem = createCartItemElement({
      id: jsonIds.id, title: jsonIds.title, price: jsonIds.price });// o clickitem vai receber essa função com esses parâmetros. Tive que fazer isso pra não dar problema de Lint. Tem que fazer isso somente pra usar o appendChild com o OL.
      // esses parâmetros desse jeito foram a forma de fazer dar certo.
      // Resumo da sequência do req. 2:  Quando eu clico no botão verde, eu chamo a função que faz a requisição e essa mesma função diz que a função createCartItemElement(que estrutura e retorna as LIs) é filha de OL. Por isso que aparece as informações do produto(que eu cliquei) naquela OL.
-  
+     
      const ol = document.querySelector(classItemCart);
       ol.appendChild(clickItem);
     localStorage.setItem('produto', ol.innerHTML); // tive que botar aqui pra começar a dar certo(ao invés de lá em cima) // isso PRECISA ser depos do ol.appendChild(clickItem). Aí por isso não é pra eu colocar return  nesse ol.appendChild
-  };
+    soma();
+  }; 
+
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -72,14 +90,14 @@ function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   return section;// esse section confuso é pra retornar ID,imagem, nome e o botão verde.
 }
 
-// function getSkuFromProductItem(item) {
-//   return item.querySelector('span.item__sku').innerText;
-// }
+//  function getSkuFromProductItem(item) {
+//    return item.querySelector('span.item__sku').innerText;
+//  }
 
 // o requisito 7 eu fiz com a ajuda do meu colega Vinicius Bodra. Link do PR dele: https://github.com/tryber/sd-010-b-project-shopping-cart/pull/118/commits/77c597dc4bcbd733bf87a272a160be9d9ef8394c
 const myFetch = () => { // essas coisas é melhor botar no final do código pra dar menos problema com o Lint.
   const p = document.createElement('p');
-  p.innerHTML = 'loading...';
+  p.innerHTML = 'Loading...'; 
   p.className = 'loading';
   document.body.appendChild(p);
   fetch('https://api.mercadolibre.com/sites/MLB/search?q=$computador')
@@ -100,5 +118,5 @@ const myFetch = () => { // essas coisas é melhor botar no final do código pra 
 window.onload = function onload() { 
   myFetch(); 
   eraseAll();
-  storage();
+ storage();
 };
